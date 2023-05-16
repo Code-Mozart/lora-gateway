0061b5c6d8a6799c6db74e6f4af0655580b69f81
capouschek <capouschek@stud.hs-heilbronn.de>
2023-05-16 17:00:35 +0200

    systeminfo job added

--- gateway/main.py
+++ gateway/main.py
@@ -5,6 +5,7 @@ from async_cron.job import CronJob
 from async_cron.schedule import Scheduler
 
 from loraImpl import LoraImpl
+from systemInfoImpl import SystemInfoImpl
 
 # pylora https://pypi.org/project/pyLoRa/
 # https://github.com/rpsreal/pySX127x
@@ -17,15 +18,17 @@ if __name__ == '__main__':
     loraImpl.lora_setup()
     loraImpl.lora_listen()
 
+    systemInfoImpl = SystemInfoImpl()
+
     scheduler = Scheduler()
-    job = CronJob(name='backendPull').every(5).second.go(loraImpl.lora_write)
-    scheduler.add_job(job)
+    jobBackend = CronJob(name='backendPull').every(5).second.go(loraImpl.lora_write)
+    jobSystemInfo = CronJob(name='systemInfo').every(60).second.go(systemInfoImpl.get_data())
+    scheduler.add_job(jobBackend)
     loop = asyncio.get_event_loop()
 
     try:
         loop.run_until_complete(scheduler.start())
     except KeyboardInterrupt:
         print("exit")
-
 
 
---
+++ gateway/systemInfoImpl.py
@@ -0,0 +1,6 @@
+class SystemInfoImpl:
+    def __init__(self):
+        pass
+
+    def get_data(self):
+        print("get data")
