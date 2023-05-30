427468ebb4274cba6c722929039d3dd42f3cf022
Luis Gutzeit <Luigutzeit@web.de>
2023-05-30 15:27:47 +0200

    fixed lora again

--- src/loraImpl.py
+++ src/loraImpl.py
@@ -14,14 +14,18 @@ class LoraImpl(LoRa):
         super(LoraImpl, self).__init__(False)
         self.lora_setup()
         self.http_impl: HttpImpl = httpImpl
+        
 
     def lora_setup(self):
         print("setup")
         self.set_freq(433.0)
-        self.set_mode(MODE.SLEEP)
-        self.set_coding_rate(8)
+        self.set_spreading_factor(8)
         self.set_bw(BW.BW125)
-        self.set_mode(MODE.STDBY)
+        self.set_mode(MODE.RXCONT)
+        print(self.get_version())
+
+        while(True):
+            time.sleep(5)
 
     def on_rx_done(self):
         # payload must be read from lora board!
@@ -29,6 +33,7 @@ class LoraImpl(LoRa):
         print("RX:")
         print(payload)
         print(codecs.decode(bytes(payload),"utf-8"))
+        #RESET module after receive!
         self.set_mode(MODE.SLEEP)
         self.reset_ptr_rx()
         BOARD.led_off()
@@ -39,12 +44,20 @@ class LoraImpl(LoRa):
         # tx?
         print("tx")
 
-    def lora_listen(self):
-        while True:
-            time.sleep(10)
-
     #def lora_write(self):
     #    # self.write_payload()
     #    print("write")
+
+    def lora_listen(self):
+        self.set_mode(MODE.RXCONT)  # receiver mode
+        time.sleep(5)
+        while True:
+            # self.read_payload()
+            # send http request to backend
+            #self.http_impl.send_data()
+            # handle request
+            #print("read")
+            time.sleep(10)
+
 
 # TODO: pip install RPi.GPIO & pip install spidev failed
